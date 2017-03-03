065d50e9959e70a8c56126590dc617f74112b912
Cuong Vu <cuongvu0103@gmail.com>
2017-03-03 15:48:25 +0700

    Check if error message is returned

--- lib/cex/transactions.rb
+++ lib/cex/transactions.rb
@@ -4,7 +4,7 @@ module Cex
       Cex.sanity_check!
       result = Cex::Net.post("/get_order_tx/", options)
       result = JSON.parse(result)
-      return unless result["ok"]
+      return if result["error"]
       order = Cex::Order.new(result["data"])
       order.transactions = result["data"]["vtx"]
       order
